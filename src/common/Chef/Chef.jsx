33195028bd22da7d27fce920d2b377ab247e618b
Borja <pintocorreas@gmail.com>
2024-02-12 12:42:36 +0100

    feat: added css and img

--- src/common/Chef/Chef.jsx
+++ src/common/Chef/Chef.jsx
@@ -2,11 +2,17 @@ import React from 'react';
 
 export const Chef = () => {
     return (
-        <div>
-            <img src="path/to/your/image.jpg" alt="Background Image" />
-            <h1>Your Text Here</h1>
+      <div className="chefHeader">
+        <div
+          className="background-chef"
+          style={{ backgroundImage: `url('../src/img/panadera.jpg')` }}
+        >
+          <div className="letter-container">
+            <h1>TEXTO</h1>
+          </div>
         </div>
+      </div>
     );
-};
+  };
 
 
