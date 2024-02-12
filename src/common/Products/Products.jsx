41ebcfa555ac6c28d6d22f9911b90a6f3d1dfd16
Borja <pintocorreas@gmail.com>
2024-02-12 19:02:01 +0100

    feat: created products component

---
+++ src/common/Products/Products.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import './Products.css';
+import coca_anchoa from '../../img/coca_anchoa.jpg';
+import coca_pi from '../../img/coca_pi.jpg'
+
+export const Products = () => {
+  return (
+    <div className="products">
+      <div className="product">
+        <img src={coca_anchoa} alt="Imagen 1" />
+        <div className="product-text">
+          <h2>Producto 1</h2>
+        </div>
+      </div>
+      <div className="product">
+        <img src={coca_pi} alt="Imagen 2" />
+        <div className="product-text">
+          <h2>Producto 2</h2>
+        </div>
+      </div>
+      <div className="product">
+        <img src="imagen3.jpg" alt="Imagen 3" />
+        <div className="product-text">
+          <h2>Producto 3</h2>
+        </div>
+      </div>
+      <div className="product">
+        <img src="imagen4.jpg" alt="Imagen 4" />
+        <div className="product-text">
+          <h2>Producto 4</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Chef } from "../../common/Chef/Chef";
+import { Products } from "../../common/products/Products";
 
 export const Home = () => {
     return (
         <div>
             <Chef />
+            <Products />
         </div>
     );
     }
