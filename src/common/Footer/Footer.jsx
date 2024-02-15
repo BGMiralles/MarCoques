01fffc5cd77b5a523f4810f6fbf67ffc944b0e1a
Borja <pintocorreas@gmail.com>
2024-02-15 12:12:18 +0100

    feat: created footed, styles and img

---
+++ src/common/Footer/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Footer.css';
+import ig from '../../img/logo-ig.png';
+
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-background">
+        {/* Imagen de fondo */}
+      </div>
+      <div className="footer-content">
+        <a href="https://www.instagram.com/restaurante_ejemplo" target="_blank" rel="noopener noreferrer">
+          <img src={ig} alt="Instagram del restaurante" />
+        </a>
+      </div>
+    </footer>
+  );
+};
+
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Chef } from "../../common/Chef/Chef";
 import { Products } from "../../common/products/Products";
+import { Footer } from "../../common/footer/footer";
 
 export const Home = () => {
     return (
         <div>
             <Chef />
             <Products />
+            <Footer />
         </div>
     );
     }
