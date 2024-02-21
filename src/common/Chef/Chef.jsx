d64d9a8d62c7b6fcc9e4e44ff6bae69443aecbf7
Borja <pintocorreas@gmail.com>
2024-02-21 14:33:41 +0100

    Actualizar imágenes en Chef.jsx y StaticNavbar.jsx

--- src/common/Chef/Chef.jsx
+++ src/common/Chef/Chef.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import './Chef.css'
+import panadera from '../../img/panadera.jpg'
 
 export const Chef = () => {
-    return (
-      <div className="chefHeader">
-        <div
-          className="background-chef"
-          style={{ backgroundImage: `url('../src/img/panadera.jpg')` }}
-        >
-          <div className="letter-container">
-            <h1>Cocas artesanas sostenibles</h1>
-          </div>
+  return (
+    <div className="chefHeader">
+      <div
+        className="background-chef"
+        style={{ backgroundImage: `url(${panadera})` }}
+      >
+        <div className="letter-container">
+          <h1>Cocas artesanas sostenibles</h1>
         </div>
       </div>
-    );
-  };
+    </div>
+  );
+};
 
 
